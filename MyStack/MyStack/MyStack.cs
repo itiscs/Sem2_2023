e47117cff69bcbbd0da21a30aa161e5720b130e4
itiscs <moudroff@gmail.com>
2023-02-14 09:37:49 +0300

    Stack<T>

---
+++ MyStack/MyStack/MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    internal class MyStack<T>  // where T :  IComparable
+    {
+        T[] mas;
+        int maxSize;
+        int top;
+
+        public MyStack(int mSize)
+        {
+            maxSize = mSize; 
+            mas = new T[maxSize];
+            top = -1;
+        }
+        public MyStack():this(10)
+        {
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public void Push(T item)
+        {
+            if (top >= maxSize-1)
+                throw new IndexOutOfRangeException("Stack is full!");
+            mas[++top] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new IndexOutOfRangeException("Stack is empty!");
+            return mas[top--];
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for(int i = 0; i <= top; i++)
+                sb.Append($"{mas[i]} - ");
+            return sb.ToString();
+        }
+    }
+}
---
+++ MyStack/MyStack/Program.cs
@@ -0,0 +1,36 @@
+﻿using MyStack;
+
+MyStack<int> st = new MyStack<int>();
+
+st.Push(1);
+st.Push(20);
+
+st.Push(30);
+st.Push(40);
+
+Console.WriteLine(st);
+
+
+Console.WriteLine(st.Pop());
+Console.WriteLine(st.Pop());
+Console.WriteLine(st.Pop());
+
+Console.WriteLine(st);
+
+MyStack<string> st2 = new MyStack<string>();
+
+st2.Push("Ivan");
+
+st2.Push("Petr");
+st2.Push("Maksim");
+
+Console.WriteLine(st2);
+
+
+Tuple<int, int> t = new Tuple<int, int>(4,5);
+
+
+
+
+
+
