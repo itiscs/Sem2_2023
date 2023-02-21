0b7e3cd06beba6a63ef76e6110bab0120f5efae2
itiscs <moudroff@gmail.com>
2023-02-21 09:26:26 +0300

    MyList<T>

---
+++ ListApp/ListApp/MyList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListApp
+{
+    public class Elem<T>
+    {
+        public T Info { get; set; }
+        public Elem<T> Next { get; set; }
+
+        public Elem(T info)
+        {
+            this.Info = info;
+        }
+    }
+
+
+    public class MyList<T>
+    {
+        private Elem<T> First { get; set; }
+
+        public int Count
+        { 
+            get 
+            {
+                int k = 0;
+                var el = First;
+                while(el != null) 
+                {
+                    k++;
+                    el = el.Next;
+                }
+                return k;
+            } 
+        }
+
+        public void AddFirst(T info)
+        {
+            var el  = new Elem<T>(info);
+            el.Next = First;
+            First = el;
+        }
+
+        public void AddLast(T info)
+        {
+            if (First == null)
+            {
+                AddFirst(info);
+                return;
+            }
+
+            var el = First;
+            while (el.Next != null)
+            {
+                el = el.Next;
+            }
+            el.Next = new Elem<T>(info);
+        }
+
+        public void DeleteFirst()
+        {
+            if (First == null)
+                return;
+
+            First = First.Next;
+        }
+
+        public void DeleteLast()
+        {
+            if (First == null)
+                return;
+            if (First.Next == null)
+            {
+                DeleteFirst();
+                return;
+            }
+            var el = First;
+            while (el.Next.Next != null)
+            {
+                el = el.Next;
+            }
+            el.Next = null;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();   
+            var el = First;
+            while(el != null)
+            {
+                sb.Append($"{el.Info.ToString()} -> ");
+                el = el.Next;
+            }
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ ListApp/ListApp/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using ListApp;
+
+var lst = new MyList<int>();
+
+Console.WriteLine(lst.Count);
+
+
+for (int i = 0; i < 10; i++)
+    lst.AddLast(i);
+
+
+
+lst.AddLast(100);
+lst.AddFirst(200);
+Console.WriteLine(lst);
+Console.WriteLine(lst.Count);
+
+for (int i = 0;i<10;i++)
+    lst.DeleteLast();
+
+Console.WriteLine(lst);
+Console.WriteLine(lst.Count);
+
+
+var lst2 = new MyList<string>();
+
+lst2.AddLast("aaa");
+lst2.AddLast("abc");
+
+lst2.AddLast("zzzz");
+
+lst2.AddLast("xxxx");
+
+Console.WriteLine(lst2);
+Console.WriteLine(lst2.Count);
+
+
+
